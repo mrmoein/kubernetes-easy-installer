58959451334e037d820405797577640c71d854d8
Moein Aghamirzaei <moein.6556@gmail.com>
2022-04-16 09:24:50 +0430

    save

--- include/functions.sh
+++ include/functions.sh
@@ -6,9 +6,26 @@ wait_for_key() {
   read -r
 }
 
-run(){
+run() {
   if [ "" != "$1" ]; then
-    magenta_print "run: $1"
+    blue_print "➜ $1"
     eval "$1"
+  fi
+}
+
+check_pkg_is_installed() {
+  PKG_OK=$(eval "$1" | grep "$2")
+  if [ "" = "$PKG_OK" ]; then
+    return 1
+  else
+    return 0
+  fi
+}
+
+echo_pkg_status() {
+  if check_pkg_is_installed "$1" "$2"; then
+    e_success "$3 are installed and work correctly"
+  else
+    e_error "$3 not installed or work correctly"
   fi
 }
